59bd1cd411563459d330ed86d16ec19241497cfb
Randy Lirano <randylirano@Randys-MBP.T-mobile.com>
2021-10-11 01:53:17 -0700

    Added css and js components

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+let rootElement = document.documentElement;
+
+function scrollToTop() {
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+
+scrollToTopBtn.addEventListener("click", scrollToTop);
